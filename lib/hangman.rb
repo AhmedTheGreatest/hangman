14cc0bb2875659349d8ca72d3484214787523e25
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-30 19:23:55 +0500

    Refactor code and add display_game method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -12,24 +12,40 @@ module Hangman
     end
 
     def new_game
-      @word = random_word
+      @correct_word = random_word.split('')
+      @word = Array.new(@correct_word.length, '_')
       play_game
     end
 
     private
 
     def play_game
-      # hello
+      # Play a game
+      display_game
+    end
+
+    def display_game
+      puts @word.join(' ')
     end
 
     def random_word
-      words = File.open(DICTIONARY_NAME).readlines
-      word = ''
-      loop do
-        word = words.sample.chomp
-        break if word.length <= 12 && word.length >= 5
+      valid_words(open_dictionary).sample.chomp
+    end
+
+    def valid_words(words)
+      words.select { |word| word.chomp.length <= 12 && word.chomp.length >= 5 }
+    end
+
+    def open_dictionary
+      begin
+        file = File.open(DICTIONARY_NAME)
+        words = file.readlines
+      rescue StandardError => e
+        puts "Error reading file: #{e.message}"
       end
-      word
+
+      file&.close
+      words
     end
   end
 end
