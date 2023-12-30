2083f624a2c1ed933315f130a78466fe3b163099
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-30 21:29:14 +0500

    Add game loading and saving (serializing) functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 module Hangman
   # This class will handle the flow of the game
+  class Menu
+    def start
+      display_start_options
+      option = gets.chomp.to_i
+      if option == 1
+        game = Game.new
+        game.new_game
+      elsif option == 2
+        game = Game.load_game("game.bin")
+        game.play_game
+      end
+    end
+
+    private
+
+    def display_start_options
+      puts 'Do you want to:'.bold.red
+      puts "#{'[1]'.cyan.bold} #{'Play a new game'.bold.cyan}"
+      puts "#{'[2]'.cyan.bold} #{'Load a previous game'.bold.cyan}"
+    end
+  end
+
   class Game
     DICTIONARY_NAME = 'dictionary.txt'
 
@@ -13,13 +37,20 @@ module Hangman
     end
 
     def new_game
+      puts 'You have chosen to start a new game!'
+      puts 'If at any time you want to save the game type save'
       @correct_word = random_word.split('')
       @word = Array.new(@correct_word.length, '_')
       @attempts = 7
       play_game
     end
 
-    private
+    def self.load_game(path)
+      # Loads a game from a file
+      File.open(path, "r") do |file|
+        Marshal.load(file.read)
+      end
+    end
 
     def play_game
       # Play a game
@@ -27,9 +58,18 @@ module Hangman
         break if game_over?
 
         display_game
-        make_guess
+        handle_input
       end
       display_game_end_msg
+    end
+
+    private
+
+    # Saves a game into a file
+    def save_game
+      File.open("game.bin", "w") do |file|
+        file.write(Marshal.dump(self))
+      end
     end
 
     def display_game_end_msg
@@ -53,8 +93,7 @@ module Hangman
       @attempts < 1
     end
 
-    def make_guess
-      guess = ask_for_guess
+    def make_guess(guess)
       if @correct_word.include?(guess)
         @correct_word.each_with_index do |letter, index|
           @word[index] = guess if letter == guess
@@ -65,14 +104,23 @@ module Hangman
       end
     end
 
-    def ask_for_guess
+    def ask_for_input
       print 'Type your guess: '
       guess = ''
       loop do
-        guess = gets.chomp
-        break unless guess.empty?
+        guess = gets.chomp.downcase
+        break unless guess.empty? && guess != 'save'
       end
-      guess[0].downcase
+      guess
+    end
+
+    def handle_input
+      input = ask_for_input
+      if input == 'save'
+        save_game
+      else
+        make_guess(input[0])
+      end
     end
 
     def display_game
@@ -104,5 +152,5 @@ module Hangman
   end
 end
 
-game = Hangman::Game.new
-game.new_game
+menu = Hangman::Menu.new
+menu.start
