6186dbceb4f048f909eeffed91750a162edf1667
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-30 20:08:58 +0500

    Add game loop

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,15 +5,17 @@ module Hangman
   class Game
     DICTIONARY_NAME = 'dictionary.txt'
 
-    def initalize
+    def initialize
       @word = []
       @correct_word = []
       @incorrect_letters = []
+      @attempts = 7
     end
 
     def new_game
       @correct_word = random_word.split('')
       @word = Array.new(@correct_word.length, '_')
+      @attempts = 7
       play_game
     end
 
@@ -21,10 +23,46 @@ module Hangman
 
     def play_game
       # Play a game
-      display_game
+      loop do
+        display_game
+        make_guess
+
+        break if game_over?
+      end
+    end
+
+    def game_over?
+      game_won? || game_lost?
+    end
+
+    def game_won?
+      @correct_word == @word
+    end
+
+    def game_lost?
+      @attempts < 1
+    end
+
+    def make_guess
+      guess = ask_for_guess
+      if @correct_word.include?(guess)
+        @correct_word.each_with_index do |letter, index|
+          @word[index] = guess if letter == guess
+        end
+      else
+        @incorrect_letters << guess
+        @attempts -= 1
+      end
+    end
+
+    def ask_for_guess
+      gets.chomp[0].downcase
     end
 
     def display_game
+      puts ''
+      puts "You have #{@attempts} attempt(s) left!"
+      puts "Incorrect letters: #{@incorrect_letters.join(' ')}"
       puts @word.join(' ')
     end
 
@@ -39,13 +77,13 @@ module Hangman
     def open_dictionary
       begin
         file = File.open(DICTIONARY_NAME)
-        words = file.readlines
+        lines = file.readlines
       rescue StandardError => e
         puts "Error reading file: #{e.message}"
       end
 
       file&.close
-      words
+      lines
     end
   end
 end
